6f9d103c23c04f4d6c208d62523c02247afa00f1
Jeremy-kr <wjddlr905@gmail.com>
2023-01-20 18:00:41 +0900

    feat: create Router
    앱에 라우터를 추가합니다.
    
    by @Jeremy-Kr

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,1 +1,14 @@
-export {};
+import { Landing } from 'components/landing';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;

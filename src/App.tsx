5bc0d3aa51820f586d067b8183077e508faa6ad5
Jeremy-kr <wjddlr905@gmail.com>
2023-01-20 20:16:49 +0900

    feat: apply theme provider
    테마 프로바이더를 사용합니다.
    글로벌 스타일을 통해 글꼴 및 CSS RESET을 적용합니다.
    
    by @Jeremy-Kr

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import Router from 'shared/Router';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from 'styles/GlobalStyle';
+import { theme } from 'styles/theme';
 
 const App = () => {
-  return <Router />;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router />
+    </ThemeProvider>
+  );
 };
 
 export default App;
---
+++ src/styles/styled.d.ts
@@ -0,0 +1,39 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      brandRed: string;
+      brandYellow: string;
+      brand100: string;
+      brand50: string;
+      brand0: string;
+      black100: string;
+      mono100: string;
+      mono60: string;
+      mono30: string;
+      mono0: string;
+      white100: string;
+    };
+    fontSize: {
+      title: string;
+      h1: string;
+      h2: string;
+      h3: string;
+      h4: string;
+      h5: string;
+      h6: string;
+      content: string;
+    };
+    lineHeight: {
+      title: string;
+      h1: string;
+      h2: string;
+      h3: string;
+      h4: string;
+      h5: string;
+      h6: string;
+      content: string;
+    };
+  }
+}

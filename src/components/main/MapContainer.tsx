871d4b18c1e22336cad71f585a506d820539cdf2
harenohee <panda4859@gmail.com>
2023-01-21 21:48:58 +0900

    fix: 지도 중심좌표 변경
    기본 위치를 팀스파르타 위경도로 변경했습니다.
    
    #6 by @harenohee

--- src/components/main/MapContainer.tsx
+++ src/components/main/MapContainer.tsx
@@ -6,7 +6,7 @@ const MapContainer = () => {
     const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
     const options = {
       //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      center: new kakao.maps.LatLng(37.502318725608795, 127.04444559443071), //지도의 중심좌표.
       level: 3, //지도의 레벨(확대, 축소 정도)
     };
 

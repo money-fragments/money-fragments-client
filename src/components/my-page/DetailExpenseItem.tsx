90461dc9ea2e29b8b66fc6ac303957660e41aad5
Jeremy-kr <wjddlr905@gmail.com>
2023-01-21 10:43:09 +0900

    feat: DetailMonthlyExpense 컴포넌트 생성
    DetailMonthlyExpense 컴포넌트의 UI를 제작합니다.
    
    by @Jeremy-Kr #15

---
+++ src/components/my-page/DetailExpenseItem.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const DetailExpenseItem = () => {
+  return (
+    <DetailMonthlyExpenseTableBody>
+      <span>21. 08. 01.</span>
+      <span>카페</span>
+      <span>아메리카노</span>
+      <span>₩ 3,000</span>
+      <span>
+        오늘 아침에 카페에서 아메리카노를 마시고 왔다. 아메리카노는 맛있었다.
+        아메리카노는 맛있었다. 아메리카노는 맛있었다. 아메리카노는 맛있었다.
+        아메리카노는 맛있었다. 아메리카노는
+      </span>
+      <div>
+        <button>지도에서 보기</button>
+        <button>e</button>
+        <button>d</button>
+      </div>
+    </DetailMonthlyExpenseTableBody>
+  );
+};
+
+const DetailMonthlyExpenseTableBody = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-bottom: 1rem;
+  text-align: center;
+
+  font-size: ${({ theme }) => theme.fontSize.content};
+  line-height: ${({ theme }) => theme.lineHeight.content};
+
+  span {
+    width: 100px;
+  }
+
+  span:nth-child(5) {
+    width: 450px;
+    max-height: 18px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  div {
+    width: 200px;
+    display: flex;
+    justify-content: space-around;
+  }
+`;
+
+export default DetailExpenseItem;

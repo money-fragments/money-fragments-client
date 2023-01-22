64ce5b5b60da84b80b41a3977d885fe6a946fd14
frank <syncstar9@gmail.com>
2023-01-22 19:05:13 +0900

    feat : Update Hover Reusable ButtonComponent  CustomButton에 Hover 기능을 추가하였습니다. @by frankkim92 #1

--- src/components/common/ButtonComponents.tsx
+++ src/components/common/ButtonComponents.tsx
@@ -13,7 +13,9 @@ interface ButtonProps {
 }
 
 export const CustomButton = styled.button<ButtonProps>`
-  border: 0.3px solid gray;
+  border: 0.5px solid gray;
+  box-shadow: 0px 2px 2px lightgray;
+
   cursor: pointer;
 
   background: ${(props) =>
@@ -34,5 +36,22 @@ export const CustomButton = styled.button<ButtonProps>`
       ? (props: any) => props.theme.fontSize[`${props.fontSize}`]
       : (props) => props.theme.fontSize.content};
 
-  border-radius: ${(props) => props.borderRadius || '15px'};
+  border-radius: ${(props) => props.borderRadius || '10px'};
+
+  /* 호버 기능을 추가했습니다.  */
+  &:hover {
+    background: ${(props) =>
+      props.backgroundColor
+        ? (props: any) => props.theme.colors[`${props.backgroundColor}`]
+        : (props) =>
+            props.theme.colors
+              .mono0}; // 왼쪽의 props.theme.colors.[mono0]에서 원하는 색상을 넣으시면 됩니다.
+
+    color: ${(props) =>
+      props.color
+        ? (props) => props.color
+        : (props) =>
+            props.theme.colors
+              .white100}; // 왼쪽의 props.theme.colors.[white100]에서 원하는 색상을 넣으시면 됩니다.
+  }
 `;
--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,7 +1,12 @@
+import { CustomButton } from 'components/common/ButtonComponents';
 import React from 'react';
 
 const Landing = () => {
-  return <div>loading..</div>;
+  return (
+    <div>
+      <h1>loading..</h1>
+    </div>
+  );
 };
 
 export default Landing;

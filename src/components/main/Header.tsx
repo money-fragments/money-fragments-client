b943cd00faf246480be427b8b4af2f22e2a4ba5d
harenohee <panda4859@gmail.com>
2023-01-24 21:06:24 +0900

    style: 로그인 버튼 스타일 수정
    추후 버튼 컴포넌트 적용 예정입니다.
    
    #13 by @harenohee

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 import { H1, H6 } from 'components/common';
+import GlobalStyle from 'styles/GlobalStyle';
 const Header = () => {
   return (
-    <Top>
-      <H1>로고</H1>
-      <button>
-        <H6>로그인/회원가입</H6>
-      </button>
-    </Top>
+    <>
+      <GlobalStyle />
+      <Top>
+        <H1>로고</H1>
+        <CustomBtn>
+          <H6>로그인/회원가입</H6>
+        </CustomBtn>
+      </Top>
+    </>
   );
 };
 
@@ -18,6 +22,16 @@ const Top = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
+`;
+
+const CustomBtn = styled.button`
+  width: 150px;
+  height: 40px;
+  margin-right: 45px;
+  background-color: ${(props) => props.theme.colors.brand0};
+  border-style: none;
+  border-radius: 27px;
 `;
 
 export default Header;

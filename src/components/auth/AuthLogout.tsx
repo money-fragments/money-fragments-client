d0538cd80f5b3dac74e55c14ba0e926f2ebe4be6
DowonKim <117058112+dollorofkorea@users.noreply.github.com>
2023-01-25 17:42:18 +0900

    Update AuthLogout.tsx
    fix : 로그아웃 ui수정

--- src/components/auth/AuthLogout.tsx
+++ src/components/auth/AuthLogout.tsx
@@ -1,7 +1,6 @@
-import { Content } from 'components/common';
+import { Content, H6 } from 'components/common';
 import { auth } from '../../firebase';
 import { signOut } from 'firebase/auth';
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import logging from './logging';
@@ -16,20 +15,63 @@ const AuthLogout = (): JSX.Element => {
 
   return (
     <LogoutContainer>
-      <Content>정말 로그아웃 하실건가요?</Content>
+      <H6>
+        돈부스라기
+        <br />
+        로그아웃 하실건가요?
+      </H6>
+
       <LogoutBtnContainer>
+        <LogoutBtnLogout onClick={() => Logout()}>Logout</LogoutBtnLogout>
         <LogoutBtnCancel onClick={() => navigate('/main')}>
           Cancel
         </LogoutBtnCancel>
-        <LogoutBtnLogout onClick={() => Logout()}>Logout</LogoutBtnLogout>
       </LogoutBtnContainer>
     </LogoutContainer>
   );
 };
 
-const LogoutContainer = styled.div``;
-const LogoutBtnContainer = styled.div``;
-const LogoutBtnCancel = styled.button``;
-const LogoutBtnLogout = styled.button``;
+const LogoutContainer = styled.div`
+  background-color: ${(props) => props.theme.colors.white60};
+  width: 350px;
+  height: 100px;
+  margin: 0 auto;
+  margin-top: 200px;
+  padding: 40px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+`;
+
+const LogoutBtnContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 30px;
+`;
+const LogoutBtnLogout = styled.button`
+  height: 30px;
+  width: 100px;
+  font-weight: 600;
+  border-radius: 10px;
+  border: 1px solid ${(props) => props.theme.colors.white0};
+  color: ${(props) => props.theme.colors.brandRed};
+  &:hover {
+    border: 3px solid ${(props) => props.theme.colors.brandRed};
+  }
+`;
+const LogoutBtnCancel = styled.button`
+  margin-left: 40px;
+  height: 30px;
+  width: 100px;
+  font-weight: 600;
+  border-radius: 10px;
+  border: 1px solid ${(props) => props.theme.colors.white0};
+  &:hover {
+    border: 3px solid ${(props) => props.theme.colors.brandRed};
+  }
+`;
 
 export default AuthLogout;

36ad5f1c2bb0e9212316cf438f1194cd11403882
Jeremy-kr <wjddlr905@gmail.com>
2023-01-25 10:40:44 +0900

    feat: add year selector in expense report
    월별 소비 리포트에서 연도 선택 인풋을 추가합니다.
    
    by @Jeremy-Kr  #31

--- src/components/my-page/ExpenseReport.tsx
+++ src/components/my-page/ExpenseReport.tsx
@@ -1,11 +1,35 @@
 import { H4 } from 'components/common';
+import useUserExpenses from 'hooks/useUserExpenses';
+import { useState } from 'react';
 import styled from 'styled-components';
 import MonthlyExpense from './MonthlyExpense';
 
 const ExpenseReport = () => {
+  const { isLoading, years } = useUserExpenses('userid1');
+  const [year, setYear] = useState<number>(
+    years[0] || new Date().getFullYear()
+  );
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <>
       <H4>월별 소비 리포트</H4>
+      <label htmlFor="year">연도선택</label>
+      <select
+        name="year"
+        id="year"
+        onSelect={() => {
+          setYear(year);
+        }}
+        value={year}
+      >
+        {years.map((year) => (
+          <option key={year} value={year}>
+            {year}
+          </option>
+        ))}
+      </select>
       <MonthlyExpenseContainer>
         <MonthlyExpense />
       </MonthlyExpenseContainer>
--- src/hooks/useUserExpenses.ts
+++ src/hooks/useUserExpenses.ts
@@ -5,6 +5,7 @@ import { getUserExpense } from 'utils/api';
 const useUserExpenses = (userId: string) => {
   const [monthlyExpense, setMonthlyExpense] = useState<number[]>([]);
   const [maxExpense, setMaxExpense] = useState<number>(0);
+  const [years, setYears] = useState<number[]>([]);
 
   const { data, isError, error, isLoading } = useQuery<Expense[], Error>(
     ['userExpenses'],
@@ -13,18 +14,24 @@ const useUserExpenses = (userId: string) => {
 
   useEffect(() => {
     const monthlyExpense: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const years: number[] = [];
     if (data) {
       data.forEach((expense) => {
         const expenseDate = new Date(+expense.date);
         const expenseMonth = expenseDate.getMonth();
         monthlyExpense[expenseMonth] += +expense.price;
+
+        if (!years.includes(expenseDate.getFullYear())) {
+          years.push(expenseDate.getFullYear());
+        }
       });
       setMonthlyExpense(monthlyExpense);
+      setYears(years);
     }
     setMaxExpense(Math.max(...monthlyExpense));
   }, [data]);
 
-  return { data, isError, error, isLoading, monthlyExpense, maxExpense };
+  return { data, isError, error, isLoading, monthlyExpense, maxExpense, years };
 };
 
 export default useUserExpenses;

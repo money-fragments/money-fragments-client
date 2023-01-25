af4657fbe71e4fae2d8e3faeff6146b5c39fc717
harenohee <panda4859@gmail.com>
2023-01-25 17:12:35 +0900

    feat: 클릭 상태 확인 state 추가
    setIsOpenListUp으로 아이콘 클릭 상태 확인합니다. true일시 컴포넌트가 보이도록 합니다.
    
    #34 by @harenohee

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Header from './Header';
-import SearchPlace from './SearchPlace';
+import ListUp from './ListUp';
+import Maps from './Maps';
 const Main = () => {
+  const [isOpenListUp, setIsOpenListUp] = useState(false);
+  const [place, setPlace] = useState('');
+
   return (
     <MainPageContainer>
-      <Header />
-      <SearchPlace />
+      <Header setIsOpenListUp={setIsOpenListUp} isOpenListUp={isOpenListUp} />
+      <MapContainer>
+        <Maps searchPlace={place} />
+        {isOpenListUp && <ListUp setPlace={setPlace} />}
+      </MapContainer>
     </MainPageContainer>
   );
 };
 
 const MainPageContainer = styled.main``;
+const MapContainer = styled.div`
+  /* width: 100%; */
+  position: relative;
+`;
 
 export default Main;

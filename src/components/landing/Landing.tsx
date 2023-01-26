9d54097bbc4d396300e25c98e6ff9fc5ac9163c1
Jeremy-kr <wjddlr905@gmail.com>
2023-01-26 21:02:41 +0900

    refactor: refactor landing page ui
    랜딩페이지의 ui를 리팩토링 합니다.
    
    by @Jeremy-Kr

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,8 +1,7 @@
-
 import styled from 'styled-components';
 import landingpage from '../assets/landingpage.png';
 
-import { H1 } from 'components/common';
+import { H1, H3 } from 'components/common';
 import { useNavigate } from 'react-router-dom';
 import { CustomButton } from 'components/common/CustomButton';
 
@@ -10,56 +9,59 @@ const Landing = () => {
   const navigate = useNavigate();
 
   const goAuth = () => {
-
     navigate('/Login');
   };
-
 
   return (
     <LandingPageContainer>
       <LandingPageTitle>
-        <H1>
-
-          반갑습니다.
-          <br />
-          돈관리 어플💸
-          <br /> 돈 부스러기 입니다
-        </H1>
+        <H1> 안녕하세요!👋</H1>
+        <H3>소비의 자취를 남기고 싶으신가요?</H3>
+        <H3>돈부스러기와 함께 소비발자국을 남겨보세요!</H3>
+        <LandingLogin
+          onClick={goAuth}
+          fontSize="h5"
+          width="120px"
+          height="40px"
+          backgroundColor="brand100"
+          color="white100"
+        >
+          함께하기
+        </LandingLogin>
       </LandingPageTitle>
-      <LandingLogin onClick={goAuth}>로그인</LandingLogin>
+      <LandingBackground />
     </LandingPageContainer>
   );
 };
 
 const LandingPageContainer = styled.div`
-  background-image: linear-gradient(
-
-      rgba(255, 255, 255, 0.6),
-      rgba(255, 255, 255, 0.6)
-
-    ),
-    url(${landingpage});
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   width: 100vw;
   height: 100vh;
+`;
+
+const LandingBackground = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  background-image: url(${landingpage});
   background-size: cover;
+  opacity: 0.3;
 `;
 
 const LandingPageTitle = styled.div`
-  margin-top: 15%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
   margin-bottom: 3%;
   text-align: center;
+  z-index: 2;
 `;
 
-const LandingLogin = styled(CustomButton)`
-  font-size: ${(props) => props.theme.fontSize.content};
-  width: 100px;
-  background-color: ${(props) => props.theme.colors.white0};
-  &:active {
-    border: 2px solid ${(props) => props.theme.colors.black100};
-  }
-`;
+const LandingLogin = styled(CustomButton)``;
 
 export default Landing;
